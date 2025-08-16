5d1ef290084d267bae6b974f8b5e566c5df55eeb
HuyHungM <136847388+HuyHungM@users.noreply.github.com>
2025-08-16 20:17:10 +0700

    Update distube.js

--- src/modules/distube.js
+++ src/modules/distube.js
@@ -1,8 +1,8 @@
 const { DisTube } = require("distube");
 const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
-const { YtDlpPlugin } = require("@distube/yt-dlp");
 const { DeezerPlugin } = require("@distube/deezer");
+const { YtDlpPlugin } = require("@distube/yt-dlp");
 const { Collection } = require("discord.js");
 const { StreamType } = require("distube");
 const ffmpegPath = require("ffmpeg-static");
@@ -18,14 +18,20 @@ module.exports = (client) => {
     emitAddSongWhenCreatingQueue: false,
     emitAddListWhenCreatingQueue: true,
     youtubeCookie: cookies,
+    searchSongs: 5,
+
     ffmpeg: {
       path: ffmpegPath,
     },
     customFilters: client.config.music.filters,
     streamType: StreamType.OPUS,
     joinNewVoiceChannel: false,
+
     ytdlOptions: {
       quality: "highestaudio",
+      requestOptions: {
+        timeout: 60000,
+      },
     },
     nsfw: true,
     plugins: [
@@ -42,7 +48,9 @@ module.exports = (client) => {
         oauthToken: process.env.SOUNDCLOUD_SECRET,
       }),
       new DeezerPlugin(),
-      new YtDlpPlugin({ update: true }),
+      new YtDlpPlugin({
+        update: true,
+      }),
     ],
   });
 
